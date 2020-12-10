b02a6a895edc552ad644752c83d0047d35064150
Sam Beckingham <sambeckingham@protonmail.com>
2020-12-10 09:09:15 +0000

    Zap zap

--- day10/day10.nim
+++ day10/day10.nim
@@ -1,27 +1,27 @@
 import algorithm, sequtils, strutils, sugar, tables
 
-var joltages = collect(newSeq):
+var ⚡s = collect(newSeq):
   for line in lines "input":
     parseInt line
 
 # Add broken port, device and sort ascending
-add joltages, 0
-add joltages, max(joltages) + 3
-sort joltages
+add ⚡s, 0
+add ⚡s, max(⚡s) + 3
+sort ⚡s
 
 var differences = collect(newSeq):
-  for i, joltage in joltages[1..^1]:
-    joltage - joltages[i]
+  for i, ⚡ in ⚡s[1..^1]:
+    ⚡ - ⚡s[i]
 
 echo "Part 1: ", differences.count(1) * differences.count(3)
 
 var optionsTable: Table[int, int]
 optionsTable[0] = 1
 
-for jolt in joltages[1 .. ^1]:
-  optionsTable[jolt] = 0
-  for ⚡ in 1..3:
-    if optionsTable.contains(jolt-⚡):
-      optionsTable[jolt] = optionsTable[jolt] + optionsTable[jolt-⚡]
+for ⚡ in ⚡s[1 .. ^1]:
+  optionsTable[⚡] = 0
+  for x in 1..3:
+    if optionsTable.contains(⚡-x):
+      optionsTable[⚡] = optionsTable[⚡] + optionsTable[⚡-x]
 
-echo "Part 2: ", optionsTable[max(joltages)]
+echo "Part 2: ", optionsTable[max(⚡s)]
